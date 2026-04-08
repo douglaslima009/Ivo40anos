0b87ea95fa0e95574bc58ccee9758087dc33ebcc
Douglas Lima. <douglaslima0929@gmail.com>
2026-04-08 01:08:48 -0300

    Quase versão final, individualizando as fontes de áuudio e provavelmente mudarei alguns modelos antes de finalizar

--- main.py
+++ main.py
@@ -160,30 +160,24 @@ def main():
     pygame.event.set_grab(True); pygame.mouse.set_visible(False)
     fonte_ui = pygame.font.SysFont('Consolas', 28, bold=True)
     
-    # --- CARREGAMENTO INDEPENDENTE DE ÁUDIOS ---
-    dialogo_blip = som_tiro = som_acerto = som_morte = None
+    dialogo_blip_inicio = dialogo_blip_final = som_tiro = som_acerto = som_morte = None
 
     try:
-        dialogo_blip = pygame.mixer.Sound("blip3.wav")
-        # Ajuste o volume aqui se ficar muito alto (0.0 a 1.0)
-        dialogo_blip.set_volume(0.8) 
-    except Exception as e:
-        print(f"Aviso: blip.wav não encontrado. {e}")
+        dialogo_blip_inicio = pygame.mixer.Sound("blip3.wav")
+        dialogo_blip_inicio.set_volume(0.8) 
+    except Exception as e: pass
 
-    try: 
-        som_tiro = pygame.mixer.Sound("shoot.wav")
-    except: 
-        pass
+    try:
+        dialogo_blip_final = pygame.mixer.Sound("blip.wav")
+        dialogo_blip_final.set_volume(0.8) 
+    except Exception as e: pass
 
-    try: 
-        som_acerto = pygame.mixer.Sound("hit.wav")
-    except: 
-        pass
-
-    try: 
-        som_morte = pygame.mixer.Sound("death.wav")
-    except: 
-        pass
+    try: som_tiro = pygame.mixer.Sound("shoot.wav")
+    except: pass
+    try: som_acerto = pygame.mixer.Sound("hit.wav")
+    except: pass
+    try: som_morte = pygame.mixer.Sound("death.wav")
+    except: pass
 
     shader_phong = compileProgram(compileShader(PHONG_VERT, GL_VERTEX_SHADER), compileShader(PHONG_FRAG, GL_FRAGMENT_SHADER))
     shader_crosshair = compileProgram(compileShader(CROSSHAIR_VERT, GL_VERTEX_SHADER), compileShader(CROSSHAIR_FRAG, GL_FRAGMENT_SHADER))
@@ -196,40 +190,42 @@ def main():
     vao_ui = criar_ui_quad_vao()
     
     glEnable(GL_DEPTH_TEST); glEnable(GL_CULL_FACE); glLineWidth(2.0)
-    
     projection = pyrr.matrix44.create_perspective_projection_matrix(45.0, largura/altura, 0.1, 100.0)
     
-    # --- MATRIZES DE TRANSFORMACAO (AJUSTE AQUI SE NECESSARIO) ---
-    
-    # HORNET: Levantando (+90 no X) e virando pra frente (+180 no Y)
+    # --- MATRIZES ---
     escala_h = pyrr.matrix44.create_from_scale([2.0, 2.0, 2.0])
-    rot_x_h = pyrr.matrix44.create_from_x_rotation(np.radians(0.0))  # Mude para -90.0 se ficar de ponta-cabeça
-    rot_y_h = pyrr.matrix44.create_from_y_rotation(np.radians(0.0)) # Mude para 0.0 se ficar de costas
-    rot_h = pyrr.matrix44.multiply(rot_x_h, rot_y_h)
-    trans_h = pyrr.matrix44.create_from_translation([0.0, -1.0, -5.0]) # Desceu um pouco no eixo Y (-1.0)
-    model_hornet = pyrr.matrix44.multiply(escala_h, rot_h)
-    model_hornet = pyrr.matrix44.multiply(model_hornet, trans_h)
+    rot_h = pyrr.matrix44.create_identity() 
+    trans_h = pyrr.matrix44.create_from_translation([0.0, -1.0, -5.0])
+    model_hornet = pyrr.matrix44.multiply(escala_h, trans_h)
     
-    # PORTA: Levantando (+90 no X)
     escala_p = pyrr.matrix44.create_from_scale([0.5, 0.5, 0.5])
-    rot_x_p = pyrr.matrix44.create_from_x_rotation(np.radians(0.0)) # Mude para -90.0 se ficar de ponta-cabeça
+    rot_p = pyrr.matrix44.create_identity() 
     trans_p = pyrr.matrix44.create_from_translation([0.0, -1.0, -25.0])
-    model_porta = pyrr.matrix44.multiply(escala_p, rot_x_p)
-    model_porta = pyrr.matrix44.multiply(model_porta, trans_p)
+    model_porta = pyrr.matrix44.multiply(escala_p, trans_p)
     
     cam_pos, cam_front, cam_up = np.array([0.0, 1.5, 2.0]), np.array([0.0, 0.0, -1.0]), np.array([0.0, 1.0, 0.0])
     yaw, pitch, sensibilidade, speed = -90.0, 0.0, 0.1, 4.0
     pygame.mouse.get_rel() 
     
-    current_room = 0 
+    current_room = 0 # 0: Dream, 1: Battle, 2: Final
     dialogo_ativo, num_inimigos_vivos = False, 0
     clock = pygame.time.Clock(); running = True
 
-    dialogos_reais = [
-        "Pequeno inseto, a fundacao esta corrompida. Para consertar este mundo, voce precisa me ajudar.",
-        "Atravesse aquela porta. O vazio esta cheio de anomalias, bugs e falhas do passado.",
-        "Destrua todos os 40 alvos para provar que voce e o verdadeiro arquiteto deste lugar!"
+    # --- LISTAS DE DIÁLOGO ---
+    dialogos_inicio = [
+        "Pequeno inseto, a fundação está corrompida. Para consertar este mundo, você precisa me ajudar.",
+        "Atravesse aquela porta. O vazio está cheio de anomalias, bugs e falhas do passado.",
+        "Destrua todos os 40 alvos para provar que você é o verdadeiro arquiteto deste lugar!"
     ]
+    
+    dialogos_final = [
+        "Inacreditável... Você conseguiu derrotar todos os 40 ecos do passado.",
+        "O caminho até aqui não foi fácil. Foi necessário muita luta e foco inabalável.",
+        "Este pequeno jogo é a nossa forma de celebrar os seus 40 anos de vida e história.",
+        "É uma alegria gigantesca ter você conosco no VORTEX como nosso coordenador. Feliz Aniversário, Ivo!"
+    ]
+    
+    dialogos_atuais = dialogos_inicio
     char_index_di, time_di, current_di = 0, 0.0, 0
     texto_atual_exibido = ""
     textura_ui = None
@@ -255,16 +251,33 @@ def main():
             if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 running = False
             
+            # --- INTERAÇÕES UNIVERSAIS NA TECLA E ---
             elif event.type == KEYDOWN and event.key == K_e:
-                if np.linalg.norm(cam_pos - np.array([0.0, 0.0, -5.0])) < 4.0 and current_room == 0:
-                    if dialogo_ativo:
-                        dialogo_ativo, char_index_di, current_di = False, 0, 0
-                    else:
-                        dialogo_ativo, char_index_di, current_di, texto_atual_exibido = True, 0, 0, ""
-                elif np.linalg.norm(cam_pos - np.array([0.0, 0.0, -25.0])) < 4.0 and current_room == 0:
-                    current_room, cam_pos = 1, np.array([0.0, 1.5, 2.0])
-                    gerar_inimigos()
+                dist_h = np.linalg.norm(cam_pos[[0,2]] - np.array([0.0, -5.0]))
+                dist_p = np.linalg.norm(cam_pos[[0,2]] - np.array([0.0, -25.0]))
+                
+                # Sala Inicial
+                if current_room == 0:
+                    if dist_h < 8.0: 
+                        if dialogo_ativo:
+                            dialogo_ativo, char_index_di, current_di = False, 0, 0
+                        else:
+                            dialogos_atuais = dialogos_inicio
+                            dialogo_ativo, char_index_di, current_di, texto_atual_exibido = True, 0, 0, ""
+                    elif dist_p < 8.0:
+                        current_room, cam_pos = 1, np.array([0.0, 1.5, 2.0])
+                        gerar_inimigos()
+                        
+                # Sala Final
+                elif current_room == 2:
+                    if dist_h < 8.0:
+                        if dialogo_ativo:
+                            dialogo_ativo, char_index_di, current_di = False, 0, 0
+                        else:
+                            dialogos_atuais = dialogos_final
+                            dialogo_ativo, char_index_di, current_di, texto_atual_exibido = True, 0, 0, ""
             
+            # --- TIRO (EXCLUSIVO NO MOUSE - BOTÃO ESQUERDO) ---
             elif event.type == MOUSEBUTTONDOWN and event.button == 1 and current_room == 1:
                 if tempo_atual - last_shot_time >= cooldown_arma:
                     last_shot_time = tempo_atual 
@@ -275,7 +288,14 @@ def main():
                             if np.dot(cam_front, to_enemy) > 0.985 and np.linalg.norm(ini['position'] - cam_pos) < 25.0: 
                                 ini['alive'], num_inimigos_vivos = False, num_inimigos_vivos - 1
                                 if som_acerto: som_acerto.play()
-                                if num_inimigos_vivos == 0 and som_morte: som_morte.play()
+                                
+                                # Terminou de matar os 40 inimigos
+                                if num_inimigos_vivos == 0:
+                                    if som_morte: som_morte.play()
+                                    current_room = 2 
+                                    # Teleporta o jogador BEM DE FRENTE para a Hornet
+                                    cam_pos = np.array([0.0, 1.5, -2.0]) 
+                                    yaw, pitch = -90.0, 0.0
                                 break 
         
         if not dialogo_ativo:
@@ -305,10 +325,10 @@ def main():
         glUniformMatrix4fv(glGetUniformLocation(shader_phong, "view"), 1, GL_FALSE, view)
         glUniform3f(glGetUniformLocation(shader_phong, "viewPos"), cam_pos[0], cam_pos[1], cam_pos[2])
         
+        # SALA INICIAL
         if current_room == 0:
             glUniformMatrix4fv(glGetUniformLocation(shader_phong, "model"), 1, GL_FALSE, model_hornet)
             for parte in vao_hornet:
-                # Mantivemos a lógica de pintura em vermelho/cinza para garantir que a cor funcione
                 glUniform3f(glGetUniformLocation(shader_phong, "objectColor"), 0.8, 0.1, 0.1) 
                 glBindVertexArray(parte['vao']); glDrawArrays(GL_TRIANGLES, 0, parte['count'])
             
@@ -317,6 +337,7 @@ def main():
                 glUniform3f(glGetUniformLocation(shader_phong, "objectColor"), 0.5, 0.5, 0.6) 
                 glBindVertexArray(parte['vao']); glDrawArrays(GL_TRIANGLES, 0, parte['count'])
             
+        # SALA DE BATALHA
         elif current_room == 1:
             for ini in inimigos:
                 if ini['alive']:
@@ -326,29 +347,42 @@ def main():
 
             glUseProgram(shader_crosshair)
             glBindVertexArray(vao_crosshair); glDrawArrays(GL_LINES, 0, num_vert_crosshair)
+            
+        # SALA FINAL DA HOMENAGEM
+        elif current_room == 2:
+            glUseProgram(shader_phong)
+            glUniformMatrix4fv(glGetUniformLocation(shader_phong, "model"), 1, GL_FALSE, model_hornet)
+            for parte in vao_hornet:
+                glUniform3f(glGetUniformLocation(shader_phong, "objectColor"), 0.8, 0.1, 0.1) 
+                glBindVertexArray(parte['vao']); glDrawArrays(GL_TRIANGLES, 0, parte['count'])
+                
+            glUseProgram(shader_crosshair)
+            glBindVertexArray(vao_crosshair); glDrawArrays(GL_LINES, 0, num_vert_crosshair)
 
-# --- SISTEMA DE UI (CAIXA DE TEXTO VISUAL E ÁUDIO) ---
-        if dialogo_ativo and current_room == 0:
+        # LÓGICA DE UI APLICADA ÀS SALAS 0 E 2
+        if dialogo_ativo and (current_room == 0 or current_room == 2):
             time_di += dt
-            texto_real = dialogos_reais[current_di]
+            texto_real = dialogos_atuais[current_di]
             
             if time_di > 0.04 and char_index_di < len(texto_real): 
                 texto_atual_exibido += texto_real[char_index_di]
                 if textura_ui is not None: glDeleteTextures(1, [textura_ui]) 
                 textura_ui = gerar_textura_texto(texto_atual_exibido, fonte_ui)
                 
-                # --- A MÁGICA DO ÁUDIO AQUI ---
-                if texto_real[char_index_di] != " " and dialogo_blip is not None:
-                    # Só toca se o canal 0 já tiver terminado de falar o blip anterior
+                # Escolhe o som correto dependendo da sala
+                som_atual = dialogo_blip_inicio if current_room == 0 else dialogo_blip_final
+                
+                if texto_real[char_index_di] != " " and som_atual is not None:
                     if not pygame.mixer.Channel(0).get_busy():
-                        pygame.mixer.Channel(0).play(dialogo_blip)
+                        pygame.mixer.Channel(0).play(som_atual)
                 
                 char_index_di += 1; time_di = 0.0
                 
             elif char_index_di == len(texto_real) and time_di > 1.0: 
-                if current_di + 1 < len(dialogos_reais):
+                if current_di + 1 < len(dialogos_atuais):
                     current_di += 1; char_index_di, time_di, texto_atual_exibido = 0, 0.0, ""
-                else: dialogo_ativo = False 
+                else: 
+                    dialogo_ativo = False 
             
             if textura_ui is not None:
                 glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); glDisable(GL_DEPTH_TEST) 
